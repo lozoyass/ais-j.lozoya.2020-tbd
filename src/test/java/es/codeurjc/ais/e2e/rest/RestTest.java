70258111c02956aa565a42105b281228cd055fd7
lozoyass <jlozoyastudillo@gmail.com>
2023-07-02 20:15:23 +0200

    recuperamos host

--- src/test/java/es/codeurjc/ais/e2e/rest/RestTest.java
+++ src/test/java/es/codeurjc/ais/e2e/rest/RestTest.java
@@ -18,17 +18,25 @@ public class RestTest {
 
     @Test
 	public void getAllBooks() throws Exception {
+        String host = System.getProperty("host");
+        org.junit.jupiter.api.Assertions.assertNotNull(host, "La propiedad 'host' no se ha especificado. Ejecuta el test con '-Dhost=<HOST>'.");
+
+        Response response = RestAssured.given().baseUri(host).get("/api/books/?topic=drama");
+        response.then().statusCode(200).contentType("application/json");
+        /*
+        RestAssured.port = 8080;
         when()
             .get("/api/books/?topic=drama").
         then()
             .assertThat()
                 .statusCode(200)
                 .contentType("application/json");
+
+         */
     }
 
     @Test
     public void sanityTest() throws Exception {
-        RestAssured.port = 8080;
         String host = System.getProperty("host");
         org.junit.jupiter.api.Assertions.assertNotNull(host, "La propiedad 'host' no se ha especificado. Ejecuta el test con '-Dhost=<HOST>'.");
 
@@ -46,4 +54,3 @@ public class RestTest {
     }
     
 }
-
