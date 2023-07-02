86edf69be2ee810c8576d5cebbdaa336b2f75b62
lozoyass <jlozoyastudillo@gmail.com>
2023-07-02 20:07:07 +0200

    quito tomcat

--- src/test/java/es/codeurjc/ais/e2e/rest/RestTest.java
+++ src/test/java/es/codeurjc/ais/e2e/rest/RestTest.java
@@ -12,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 @DisplayName("REST tests")
 public class RestTest {
 
