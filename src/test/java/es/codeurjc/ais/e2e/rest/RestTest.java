ec8e1302e240db5783674219eb91e0214dcb9837
Danitutan9 <dani.garcia.redondo.0208@gmail.com>
2023-07-02 13:14:40 +0200

    sanity

--- src/test/java/es/codeurjc/ais/e2e/rest/RestTest.java
+++ src/test/java/es/codeurjc/ais/e2e/rest/RestTest.java
@@ -52,46 +52,6 @@ public class RestTest {
         // Verificar la longitud de la descripción
         org.junit.jupiter.api.Assertions.assertTrue(description.length() <= 953, "La descripción del libro es mayor a 953 caracteres");
 
-
-        /*
-        when().get("/api/books/OL27479W").then().assertThat().statusCode(200).contentType("application/json");
-
-        String host = System.getProperty("host");
-        org.junit.jupiter.api.Assertions.assertNotNull(host, "La propiedad 'host' no se ha especificado. Ejecuta el test con '-Dhost=<HOST>'.");
-        
-        String bookId = "OL27479W";
-        String url = host + "api/books/OL27479W";
-
-        int maxRetries = 3;
-        int retryDelayMillis = 2000;
-        int responseCode = 0;
-        String response = "";
-
-        for (int i = 0; i < maxRetries; i++) {
-            try {
-                java.net.URL urlObj = new java.net.URL(url);
-                java.net.HttpURLConnection connection = (java.net.HttpURLConnection) urlObj.openConnection();
-                connection.setRequestMethod("GET");
-                responseCode = connection.getResponseCode();
-                response = connection.getResponseMessage();
-                if (responseCode == 200) {
-                    break;
-                }
-            } catch (java.io.IOException e) {
-                // Retry after delay
-
-                try {
-                    Thread.sleep(retryDelayMillis);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }
-
-        org.junit.jupiter.api.Assertions.assertEquals(200, responseCode, "La solicitud no devuelve un código 200 OK");
-        org.junit.jupiter.api.Assertions.assertTrue(response.length() <= 953, "La descripción del libro es mayor a 953 caracteres");
-        */
-
     }
     
 }
